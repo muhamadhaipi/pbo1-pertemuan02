16db5b9f43112b5e8e7e6c9f6765199ace8bc0ef
muhamdhaipi <Syarpanimuhamad@gmail.com>
2024-04-19 22:37:00 +0800

    Memanggil method dari Class di package lain

--- model/MataKuliah.java
+++ model/MataKuliah.java
@@ -5,7 +5,7 @@ public class MataKuliah{
   System.out.println();
   System.out.println("================ Data Mata Kuliah ==============");
   System.out.println("KOde  : TIF3402");
-  System.out.println("Nama  : Pemrograman Berbasis Objek");
+  System.out.println("Nama  : Pemrograman Berbasis Objek 1");
   System.out.println("================================================");
  }
 }
